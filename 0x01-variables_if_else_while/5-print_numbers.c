ac617342691982cecb2f85d46035b644b7c1ac8f
okenglish.teacher@gmail.com <okenglish.teacher@gmail.com>
2023-01-28 15:21:13 -0800

    Numerize

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+  * main - entry point 
+  *
+  * Return: Always 0
+  */
+
+int main(void)
+{
+	int num;
+
+	for (num = '0' ; num <= '9' ; num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+	return (0);
+}
