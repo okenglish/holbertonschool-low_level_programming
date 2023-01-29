653f7b0929a05e98e78fddb9be08e16385697cb2
okenglish.teacher@gmail.com <okenglish.teacher@gmail.com>
2023-01-28 16:34:55 -0800

    yep

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+  * main - Entry point
+  *
+  * Return - Always 0
+  */
+
+int main(void)
+
+{
+	int c;
+	for (c = 0; c <= 9; c++)
+	{
+		putchar('0' + c);
+		if (c != 9)
+		{
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
